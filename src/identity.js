8cc9145d6093e902527510a52fa61cfdfb2868f6
Misha Tavkhelidze <misha.tavkhelidze@gmail.com>
2017-03-16 12:22:58 +0400

    Add `identity`

---
+++ src/identity.js
@@ -0,0 +1,15 @@
+/**
+ * Identity function
+ *
+ * Returns whatever is it's first argument.
+ *
+ * Written by Misha Tavkhelidze <misha.tavkhelidze@gmail.com>
+ *
+ * Copyright (c) 2017 Misha Tavkhelidze
+ */
+
+const identity = x => x;
+
+export {
+    identity,
+};
