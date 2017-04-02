009c50f219921bf6a132534b93c0e3b1f631b2e2
Misha Tavkhelidze <misha.tavkhelidze@gmail.com>
2017-04-02 10:35:21 +0400

    Add copyright notice

--- src/ds/List.test.js
+++ src/ds/List.test.js
@@ -1,3 +1,29 @@
+/**
+ * File List.test.js is part of alef.js library.
+ *
+ * Written by Misha Tavkhelidze <misha.tavkhelidze@gmail.com>
+ *
+ * Copyright (c) 2017 Misha Tavkhelidze
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 import test from 'ava';
 import { List } from './List';
 
@@ -22,7 +48,7 @@ test('is iterable in correct order', t => {
     const etalon = [1, '0xdeadbeef'];
     const result = [];
     const xs = new List(1, '0xdeadbeef');
-    for (let x of xs) {
+    for (const x of xs) {
         result.push(x);
     }
     for (let i = 0; i < result.length; i += 1) {
