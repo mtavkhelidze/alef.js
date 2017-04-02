0bee4b729d754ed6fb7608e88c2b59da21959103
Misha Tavkhelidze <misha.tavkhelidze@gmail.com>
2017-04-02 09:53:13 +0400

    Fix List test names

--- src/ds/List.test.js
+++ src/ds/List.test.js
@@ -3,22 +3,22 @@ import { List } from './List';
 
 const log = console.log.bind(console);
 
-test('empty list head() return undefined', t => {
+test('#head of empty returns undefined', t => {
     const xs = new List();
     t.is(undefined, xs.head());
 });
 
-test('list with one number head() returns that number', t => {
+test('with one number #head returns that number', t => {
     const xs = new List(1);
     t.is(1, xs.head());
 });
 
-test('list with one string head() returns that string', t => {
+test('with one string #head returns that string', t => {
     const xs = new List('0xdeadbeef');
     t.is('0xdeadbeef', xs.head());
 });
 
-test('list is iterable and in correct order', t => {
+test('is iterable in correct order', t => {
     const etalon = [1, '0xdeadbeef'];
     const result = [];
     const xs = new List(1, '0xdeadbeef');
