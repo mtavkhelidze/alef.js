382f3a3d905ccf8ab5aeb1674d753b9456e7edf6
Misha Tavkhelidze <misha.tavkhelidze@gmail.com>
2017-04-02 10:39:54 +0400

    Add LICENSE file

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/**
+ *
+ * Written by Misha Tavkhelidze <misha.tavkhelidze@gmail.com>
+ * Copyright (c) 2017 Misha Tavkhelidze
+ */
+
+import 'babel-polyfill';
+
 import { identity } from './src/fn/identity';
 import { map } from './src/fn/map';
 import { unless } from './src/fn/unless';
